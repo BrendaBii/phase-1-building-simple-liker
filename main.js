23d1e07f82f57cfdeb4f3950aa897203301c50ce
BrendaBii <bii.brenda.bcb@gmail.com>
2023-07-09 13:08:14 +0300

    Completed assignment.

--- main.js
+++ main.js
@@ -3,8 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let emptyHearts = document.getElementsByClassName("like-glyph");
+let modal = document.getElementById("modal");
 
+for (let item of emptyHearts){
+  item.addEventListener("click", likeMe);
 
+  function likeMe(){
+    let response = mimicServerCall();
+
+    response.then(() => {
+      if(item.innerHTML === EMPTY_HEART){
+        item.innerHTML = FULL_HEART;
+        item.setAttribute("class", "activated-heart")
+      }else{
+        item.innerHTML = EMPTY_HEART;
+        item.setAttribute("class", "like-glyph");
+      }
+      
+    })
+
+    response.catch((error) => {
+      modal.removeAttribute("class");
+      document.getElementById("modal-message").innerHTML = error;
+      setTimeout(hideModal, 3000);
+      console.log(item)
+
+      function hideModal(){
+        modal.setAttribute("class", "hidden");
+      }
+    })
+  }
+}
 
 
 //------------------------------------------------------------------------------
